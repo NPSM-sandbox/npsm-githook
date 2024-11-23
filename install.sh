55d619cac98b9fecfddb581af71f3fcda10b0c14
YanaKo <91898700+Yana94Ko@users.noreply.github.com>
2024-11-23 12:33:47 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,53 +58,7 @@ main() {
     fi
 
     # step 2 : Define pre commit hook
-    HOOK_SCRIPT='COMMIT_MESSAGE_FILE_PATH=$1
-
-      #branch_name에서 commit message 를 자동으로 생성해주는 훅 입니다
-      #branch_name 예시       :    feat/1-init-project
-      #commit_msg 입력 예시    :    init : init kopring project 
-      #commit_msg 출력 예시    :    #1 init: init kopring project 
-      
-      #merge는 제외합니다
-      MERGE=$(grep -c -i 'merge' < "$COMMIT_MESSAGE_FILE_PATH")
-      if [ "$MERGE" != "0" ] ; then
-        exit 0
-      fi
-      
-      VALID_KEYWORDS=(init feat fix refactor test docs style chore merge)
-      
-      PREFIX=$(git branch | grep '\*' | sed 's/* //' | sed 's/^.*\///' | sed 's/^\([^-]*\).*/\1/' | sed 's/^[^/]*\///')
-      if ! [[ $PREFIX =~ ^[0-9]+$ ]]; then
-          PREFIX=""
-      fi
-      
-      SECOND_PREFIX=$(cat "$COMMIT_MESSAGE_FILE_PATH" | sed -n 's/^\([^:]*\):.*$/\1/p' | sed 's/ *$//')
-      if [ "$(echo "$SECOND_PREFIX" | cut -c 1)" = " " ]; then
-          SECOND_PREFIX=$(echo "$SECOND_PREFIX" | sed 's/^ *//')
-      fi
-      if [ -z "$SECOND_PREFIX" ]; then
-        SECOND_PREFIX="null"
-      fi
-      
-      if [ $(printf "%s\n","${VALID_KEYWORDS[@]}" | grep -c "$SECOND_PREFIX") -eq 0 ]; then
-        echo "올바른 키워드를 사용해 주세요. 현재 입력된 키워드:\""$SECOND_PREFIX"\""
-        exit 1
-      fi
-      
-      EXISTING_MESSAGE=$(cat "$COMMIT_MESSAGE_FILE_PATH" | sed "s/$SECOND_PREFIX//g" | sed 's/://g')
-      if [ -n "$PREFIX" ]; then
-        EXISTING_MESSAGE=$(echo "$EXISTING_MESSAGE" | sed "s/\[$PREFIX\]//g" | sed "s/$PREFIX//g")
-      fi
-      if [ "$(echo "$EXISTING_MESSAGE" | cut -c 1)" = " " ]; then
-          EXISTING_MESSAGE=$(echo "$EXISTING_MESSAGE" | sed 's/^ *//')
-      fi
-      
-      if [ -z "$PREFIX" ]; then
-        echo "$SECOND_PREFIX: $EXISTING_MESSAGE" > "$COMMIT_MESSAGE_FILE_PATH"
-      else
-        echo "#$PREFIX $SECOND_PREFIX: $EXISTING_MESSAGE" > "$COMMIT_MESSAGE_FILE_PATH"
-      fi
-    '
+    HOOK_SCRIPT='COMMIT_MESSAGE_FILE_PATH=$1\n\n#branch_name에서 commit message 를 자동으로 생성해주는 훅 입니다\n#branch_name 예시       :    feat/1-init-project\n#commit_msg 입력 예시    :    init : init kopring project \n#commit_msg 출력 예시    :    #1 init: init kopring project \n\n#merge는 제외합니다\nMERGE=$(grep -c -i 'merge' < "$COMMIT_MESSAGE_FILE_PATH")\nif [ "$MERGE" != "0" ] ; then\n\texit 0\nfi\n\nVALID_KEYWORDS=(init feat fix refactor test docs style chore merge)\n\nPREFIX=$(git branch | grep '\*' | sed 's/* //' | sed 's/^.*\///' | sed 's/^\([^-]*\).*/\1/' | sed 's/^[^/]*\///')\nif ! [[ $PREFIX =~ ^[0-9]+$ ]]; then\n\tPREFIX=""\nfi\n\nSECOND_PREFIX=$(cat "$COMMIT_MESSAGE_FILE_PATH" | sed -n 's/^\([^:]*\):.*$/\1/p' | sed 's/ *$//')\nif [ "$(echo "$SECOND_PREFIX" | cut -c 1)" = " " ]; then\n\tSECOND_PREFIX=$(echo "$SECOND_PREFIX" | sed 's/^ *//')\nfi\nif [ -z "$SECOND_PREFIX" ]; then\n\tSECOND_PREFIX="null"\nfi\n\nif [ $(printf "%s\n","${VALID_KEYWORDS[@]}" | grep -c "$SECOND_PREFIX") -eq 0 ]; then\n\techo "올바른 키워드를 사용해 주세요. 현재 입력된 키워드:\""$SECOND_PREFIX"\""\n\texit 1\nfi\n\nEXISTING_MESSAGE=$(cat "$COMMIT_MESSAGE_FILE_PATH" | sed "s/$SECOND_PREFIX//g" | sed 's/://g')\nif [ -n "$PREFIX" ]; then\n\tEXISTING_MESSAGE=$(echo "$EXISTING_MESSAGE" | sed "s/\[$PREFIX\]//g" | sed "s/$PREFIX//g")\nfi\nif [ "$(echo "$EXISTING_MESSAGE" | cut -c 1)" = " " ]; then\n\tEXISTING_MESSAGE=$(echo "$EXISTING_MESSAGE" | sed 's/^ *//')\nfi\n\nif [ -z "$PREFIX" ]; then\n\techo "$SECOND_PREFIX: $EXISTING_MESSAGE" > "$COMMIT_MESSAGE_FILE_PATH"\nelse\n\techo "#$PREFIX $SECOND_PREFIX: $EXISTING_MESSAGE" > "$COMMIT_MESSAGE_FILE_PATH"\nfi\n'
 
     # step 3 : Define the path to the Git hooks directory
     REPO_PATH="$(git rev-parse --show-toplevel)"
